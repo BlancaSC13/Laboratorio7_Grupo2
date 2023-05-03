10feb0fb6073304a77104f8ba6d95a4393be282c
Gilberth Chaves A <gchavesav@gmail.com>
2023-05-03 14:34:02 -0600

    Lab7 - ArrayQueue, LinkedQueue and HeaderLinkedQueue

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -1,14 +1,52 @@
-package ucr.lab;
+package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
+import ucr.lab.HelloApplication;
+
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private BorderPane bp;
+    @FXML
+    private AnchorPane ap;
+    @FXML
+    private Text txtMessage;
+
+    private void loadPage(String page) {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(page));
+        try {
+            this.bp.setCenter(fxmlLoader.load());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    void Exit(ActionEvent event) {
+        System.exit(0);
+    }
+
+    @FXML
+    void Home(ActionEvent event) {
+        this.txtMessage.setText("Laboratory No. 7");
+        this.bp.setCenter(ap);
+    }
+
+    @FXML
+    void priorityQueueOnAction(ActionEvent event) {
+        loadPage("priorityQueue.fxml");
+    }
+
+    @FXML
+    void queueStackOnAction(ActionEvent event) {
+        loadPage("queueStack.fxml");
     }
 }
